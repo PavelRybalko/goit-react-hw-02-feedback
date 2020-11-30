b5c8876cea8c58d86db9af7747e69e6345c79a01
PavelRybalko <freedom277f@gmail.com>
2020-11-30 20:17:06 +0200

    First step

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ReviewsCounter from './components/ReviewsCounter/ReviewsCounter';
+
+export default function App() {
+  return (
+    <>
+      <ReviewsCounter
+      // good={0}
+      // neutral={0}
+      // bad={0}
+      />
+    </>
+  );
+}
